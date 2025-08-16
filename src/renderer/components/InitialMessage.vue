68e86a75e412b9dad51ecf64d032e204f0b2ec6f
gyuhyeong <kgh0730@gmail.com>
2025-08-16 19:01:29 +0900

    Feat: Initial message 컴포넌트 구현

---
+++ src/renderer/components/InitialMessage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center text-gray-500 mt-16">
+    <p>궁금한 점을 입력하고 'AI로 답변 생성' 버튼을 눌러보세요.</p>
+    <p class="text-sm">ClariFact AI가 정보를 종합하고 정제하여 보여드립니다.</p>
+  </div>
+</template>
+<script setup>
+</script>
